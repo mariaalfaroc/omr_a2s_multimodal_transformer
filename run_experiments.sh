a8c3e4e7525cf7b50c4d3ced78801b75eab8df33
mariaalfaroc <mac77@alu.ua.es>
2024-02-08 12:23:26 +0100

    using distorted images in the experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -8,10 +8,10 @@
 
 for input_modality in image audio; do
     for train_ds in grandstaff beethoven chopin hummel joplin mozart scarlatti-d; do
-        python -u train.py --ds_name $train_ds --krn_encoding kern --input_modality $input_modality --attn_window 100 --epochs 300 --patience 5 --batch_size 1 
+        python -u train.py --ds_name $train_ds --krn_encoding kern --input_modality $input_modality --attn_window 100 --epochs 300 --patience 5 --batch_size 1  --use_distorted_images
         for test_ds in grandstaff beethoven chopin hummel joplin mozart scarlatti-d; do
             if [ $train_ds != $test_ds ]; then
-                python -u test.py --ds_name $test_ds --krn_encoding kern --input_modality $input_modality --checkpoint_path weights/$train_ds/$input_modality\_kern.ckpt
+                python -u test.py --ds_name $test_ds --krn_encoding kern --input_modality $input_modality --checkpoint_path weights/$train_ds/$input_modality\_kern.ckpt  --use_distorted_images
             fi
         done
     done
