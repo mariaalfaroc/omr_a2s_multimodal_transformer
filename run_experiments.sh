53c59576d3c7453086f43569ab05d3ed9bebe6a8
mariaalfaroc <mac77@alu.ua.es>
2024-02-15 16:25:55 +0100

    update checkpoint path depending of the modadility

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,7 +2,7 @@
 
 
 # NOTE:
-# 1) Not using distorted images for now
+# 1) Using distorted images and clean audios for training and testing
 # 2) bekern and kern results in the same sequence after the parser
 # so we only need to run one of them -> kern
 
@@ -11,7 +11,11 @@ for input_modality in image audio; do
         python -u train.py --ds_name $train_ds --krn_encoding kern --input_modality $input_modality --attn_window 100 --epochs 300 --patience 5 --batch_size 1  --use_distorted_images
         for test_ds in grandstaff beethoven chopin hummel joplin mozart scarlatti-d; do
             if [ $train_ds != $test_ds ]; then
-                python -u test.py --ds_name $test_ds --krn_encoding kern --input_modality $input_modality --checkpoint_path weights/$train_ds/$input_modality\_kern.ckpt  --use_distorted_images
+                if [ $input_modality == "image" ]; then
+                    checkpoint_path=weights/$train_ds/image_distorted_kern.ckpt
+                else
+                    checkpoint_path=weights/$train_ds/audio_kern.ckpt
+                python -u test.py --ds_name $test_ds --krn_encoding kern --input_modality $input_modality --checkpoint_path $checkpoint_path  --use_distorted_images
             fi
         done
     done
